7d3af423e98c1a3f16620c59663a12237b8884ac
Pier-Hugues Pellerin <phpellerin@gmail.com>
2017-05-16 15:14:46 -0400

    Making the plugin build system a more robust
    Allow to build the logstash with the following matrix:
    
    - Release Gems
    - Unreleased gems on 5.x
    - Master
    - Specific jruby 9k

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in logstash-mass_effect.gemspec
 gemspec
+
+if ENV["LOGSTASH_BRANCH"]
+  gem 'logstash-core', :path => './logstash/logstash-core'
+end
---
+++ ci/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ev
+
+if [ "$LOGSTASH_BRANCH" ]; then
+    git clone https://github.com/elastic/logstash.git $BRANCH --depth 1
+    cd ./logstash
+    # We need to build the jars for that specific version
+    gradle assemble
+    cd ../
+fi
+
+bundle exec rake test:integration:setup
+bundle exec rake vendor
+./gradlew test
+bundle exec rspec spec
+bundle exec rspec spec --tag integration
